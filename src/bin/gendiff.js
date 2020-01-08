8d7a6f566faad8b89c81922bb26782c54ae16911
mbalyura <mbalyura@gmail.com>
2020-01-08 22:21:42 +0300

    step 2 add program opts&args

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,6 +4,13 @@ import program from 'commander';
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => console.log(firstConfig, secondConfig))
   .parse(process.argv);
 
 console.log('app is running');
+
+console.log(program.format);
+
+console.log(program.opts());
