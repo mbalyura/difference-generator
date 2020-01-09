0279ab5e8eaa110c67950e6aba740742d7a308bb
mbalyura <mbalyura@gmail.com>
2020-01-09 21:01:07 +0300

    step 4 jest tests add

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 start:
-	npx babel-node -- src/bin/gendiff.js __test__/before.json __test__/after.json
+	npx babel-node -- src/bin/gendiff.js __fixtures__/before.json __fixtures__/after.json
 publish:
 	rm -rf dist
 	npm publish --dry-run
 	npm link
 lint:
 	npx eslint .
+test:
+	npx jest
+watch:
+	npx jest --watch
---
+++ __test__/index.test.js
@@ -0,0 +1,12 @@
+import fs from 'fs';
+import genDiff from '../src';
+
+test('two plain json', () => {
+  const pathToBefore = '__fixtures__/before.json';
+  const pathToAfter = '__fixtures__/after.json';
+  const result = fs.readFileSync('__fixtures__/result.txt', 'utf-8');
+  expect(genDiff(pathToBefore, pathToAfter)).toEqual(result);
+  expect(genDiff(pathToBefore, pathToAfter).includes('host: hexlet.io\n')).toBe(true);
+  expect(genDiff(pathToBefore, pathToAfter).includes('+ timeout: 20\n')).toBe(true);
+  expect(genDiff(pathToBefore, pathToAfter).includes('- follow: false\n')).toBe(true);
+});
